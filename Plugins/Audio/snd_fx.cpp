1d93791aa34e787fd12787b1db87eb6893050db6
LAGonauta <lagonauta@gmail.com>
2018-06-09 06:57:18 -0300

    Change EFX to better match HL EAX.
    Still needs hand-tuning.

--- Plugins/Audio/snd_fx.cpp
+++ Plugins/Audio/snd_fx.cpp
@@ -5,6 +5,7 @@
 #include "snd_local.h"
 #include "zone.h"
 #include <math.h>
+#include "OpenAL/efx-presets.h"
 
 extern cvar_t *sxroom_off;
 extern cvar_t *sxroomwater_type;
@@ -14,137 +15,148 @@ ALuint alUnderWaterFilter;
 ALuint alReverbEffects[CSXROOM];
 ALuint alAuxEffectSlots;
 
-typedef struct
-{
-	float size;
-	float feedback;
-	int lowpass;
-	float decay;
-	float delay;
-	float mod;;
-}al_room_t;
-
-al_room_t presets_room[CSXROOM] = {
-	{0, 0, 1, 0, 0, 0},
+EFXEAXREVERBPROPERTIES presets_room[CSXROOM] = {
+    EFX_REVERB_PRESET_GENERIC,                    //  0
 	//SXROOM_GENERIC
-	{0, 0.1, 1, 0, 0.065, 0},
+    EFX_REVERB_PRESET_GENERIC,                    //  1
 	//SXROOM_METALIC_S
-	{0, 0.75, 1, 0, 0.02, 0},
-	{0, 0.78, 1, 0, 0.03, 0},
-	{0, 0.77, 1, 0, 0.06, 0},
+    EFX_REVERB_PRESET_PIPE_SMALL,                 //  2
+    EFX_REVERB_PRESET_PIPE_LONGTHIN,              //  3
+    EFX_REVERB_PRESET_PIPE_RESONANT,              //  4
 	//SXROOM_TUNNEL_S
-	{0.05, 0.7, 1, 0.85, 0.018, 0},
-	{0.05, 0.7, 1, 0.85, 0.020, 0},
-	{0.05, 0.7, 1, 0.85, 0.025, 0},
+    EFX_REVERB_PRESET_HALLWAY,                    //  4
+    EFX_REVERB_PRESET_CASTLE_HALL,                //  6
+    EFX_REVERB_PRESET_FACTORY_HALL,               //  7
 	//SXROOM_CHAMBER_S
-	{0.05, 0, 1, 0.84, 0, 0},
-	{0.05, 0, 1, 0.90, 0, 0},
-	{0.05, 0, 1, 0.95, 0, 0},
+    EFX_REVERB_PRESET_SPACESTATION_SMALLROOM,     //  8
+    EFX_REVERB_PRESET_SPACESTATION_MEDIUMROOM,    //  9
+    EFX_REVERB_PRESET_SPACESTATION_LARGEROOM,     // 10
 	//SXROOM_BRITE_S
-	{0.05, 0, 0, 0.7, 0, 0},
-	{0.05, 0, 0, 0.78, 0, 0},
-	{0.05, 0, 0, 0.86, 0, 0},
+    EFX_REVERB_PRESET_CASTLE_SMALLROOM,           // 11
+    EFX_REVERB_PRESET_CASTLE_MEDIUMROOM,          // 12
+    EFX_REVERB_PRESET_CASTLE_LARGEROOM,           // 13
 	//SXROOM_WATER1
-	{0, 0, 1, 0, 0, 0},
-	{0, 0.85, 1, 0, 0.06, 0},
-	{0, 0.6, 1, 0, 0.2, 0},
+    EFX_REVERB_PRESET_UNDERWATER,                 // 14
+    EFX_REVERB_PRESET_UNDERWATER,                 // 15
+    EFX_REVERB_PRESET_UNDERWATER,                 // 16
 	//SXROOM_CONCRETE_S
-	{0.05, 0.48, 1, 0.8, 0, 0},
-	{0.06, 0.52, 1, 0.9, 0, 0},
-	{0.07, 0.6, 1, 0.94, 0.3, 0},
+    EFX_REVERB_PRESET_PREFAB_WORKSHOP,            // 17
+    EFX_REVERB_PRESET_PREFAB_SCHOOLROOM,          // 18
+    EFX_REVERB_PRESET_PREFAB_PRACTISEROOM,        // 19
 	//SXROOM_OUTSIDE1
-	{0, 0.42, 1, 0, 0.3, 0},
-	{0, 0.48, 1, 0, 0.35, 0},
-	{0, 0.6, 1, 0, 0.38, 0},
+    EFX_REVERB_PRESET_OUTDOORS_BACKYARD,                     // 20
+    EFX_REVERB_PRESET_OUTDOORS_DEEPCANYON,                      // 21
+    EFX_REVERB_PRESET_OUTDOORS_CREEK,                       // 22
 	//SXROOM_CAVERN_S
-	{0.05, 0.28, 1, 0.9, 0.2, 0},
-	{0.07, 0.4, 1, 0.9, 0.3, 0},
-	{0.09, 0.5, 1, 0.9, 0.35, 0},
+    EFX_REVERB_PRESET_CAVE,                       // 23
+    EFX_REVERB_PRESET_CAVE,                       // 24
+    EFX_REVERB_PRESET_CAVE,                       // 25
 	//SXROOM_WEIRDO1
-	{0.01, 0, 0, 0.9, 0, 1.0},
-	{0.01, 0.999, 1, 0, 0.009, 0},
-	{0.01, 0.8, 0, 0.999, 0.2, 0}
+    EFX_REVERB_PRESET_MOOD_HEAVEN,                // 26
+    EFX_REVERB_PRESET_MOOD_HELL,                  // 27
+    EFX_REVERB_PRESET_MOOD_MEMORY                 // 28
 };	
 
 void SX_ApplyEffect(aud_channel_t *ch, int roomtype, qboolean underwater)
 {
-	if(!qal_efxinit)
-		return;
+    if (!qal_efxinit)
+        return;
 
-	if(roomtype > 0 && roomtype < CSXROOM && sxroom_off && !sxroom_off->value)
-	{
-		qalAuxiliaryEffectSloti(alAuxEffectSlots, AL_EFFECTSLOT_EFFECT, alReverbEffects[roomtype]);
-		if(underwater)
-		{
-			qalSource3i(ch->source, AL_AUXILIARY_SEND_FILTER, alAuxEffectSlots, 0, alUnderWaterFilter);
-			qalSourcei(ch->source, AL_DIRECT_FILTER, alUnderWaterFilter);
-		}
-		else
-		{
-			qalSource3i(ch->source, AL_AUXILIARY_SEND_FILTER, alAuxEffectSlots, 0, 0);
-			qalSourcei(ch->source, AL_DIRECT_FILTER, 0);
-		}
-	}
-	else
-	{
-		qalSource3i(ch->source, AL_AUXILIARY_SEND_FILTER, 0, 0, 0);
-		if(underwater)
-			qalSourcei(ch->source, AL_DIRECT_FILTER, alUnderWaterFilter);
-		else
-			qalSourcei(ch->source, AL_DIRECT_FILTER, 0);
-	}
+    if (roomtype > 0 && roomtype < CSXROOM && sxroom_off && !sxroom_off->value)
+    {
+        if (underwater)
+        {
+            qalAuxiliaryEffectSloti(alAuxEffectSlots, AL_EFFECTSLOT_EFFECT, alReverbEffects[14]);
+            qalSource3i(ch->source, AL_AUXILIARY_SEND_FILTER, alAuxEffectSlots, 0, alUnderWaterFilter);
+            qalSourcei(ch->source, AL_DIRECT_FILTER, alUnderWaterFilter);
+        }
+        else
+        {
+            qalAuxiliaryEffectSloti(alAuxEffectSlots, AL_EFFECTSLOT_EFFECT, alReverbEffects[roomtype]);
+            qalSource3i(ch->source, AL_AUXILIARY_SEND_FILTER, alAuxEffectSlots, 0, 0);
+            qalSourcei(ch->source, AL_DIRECT_FILTER, 0);
+        }
+    }
+    else
+    {
+        qalSource3i(ch->source, AL_AUXILIARY_SEND_FILTER, 0, 0, 0);
+        if (underwater)
+            qalSourcei(ch->source, AL_DIRECT_FILTER, alUnderWaterFilter);
+        else
+            qalSourcei(ch->source, AL_DIRECT_FILTER, 0);
+    }
 }
 
-ALboolean SX_InitReverbEffect(al_room_t *RoomParam, ALuint uiEffect)
+ALboolean SX_InitReverbEffect(EFXEAXREVERBPROPERTIES *RoomParam, ALuint uiEffect)
 {
-	ALboolean bReturn = AL_FALSE;
+    ALboolean bReturn = AL_FALSE;
 
-	qalGetError();
+    qalGetError();
 
-	qalEffecti(uiEffect, AL_EFFECT_TYPE, AL_EFFECT_EAXREVERB );
-	qalEffectf(uiEffect, AL_EAXREVERB_DENSITY, min( pow(RoomParam->size, 3.0f) * 0.0625, 1) );
-	qalEffectf(uiEffect, AL_EAXREVERB_GAINLF, RoomParam->lowpass ? 0.25 : AL_EAXREVERB_DEFAULT_GAINLF);
-	qalEffectf(uiEffect, AL_EAXREVERB_DECAY_TIME, RoomParam->decay);
-	qalEffectf(uiEffect, AL_EAXREVERB_REFLECTIONS_DELAY, RoomParam->delay);
-	qalEffectf(uiEffect, AL_EAXREVERB_REFLECTIONS_GAIN, RoomParam->feedback);
-	qalEffectf(uiEffect, AL_EAXREVERB_MODULATION_DEPTH, RoomParam->mod);
+    qalEffecti(uiEffect, AL_EFFECT_TYPE, AL_EFFECT_EAXREVERB);
+    qalEffectf(uiEffect, AL_EAXREVERB_DENSITY, RoomParam->flDensity);
+    qalEffectf(uiEffect, AL_EAXREVERB_DIFFUSION, RoomParam->flDiffusion);
+    qalEffectf(uiEffect, AL_EAXREVERB_GAIN, RoomParam->flGain);
+    qalEffectf(uiEffect, AL_EAXREVERB_GAINHF, RoomParam->flGainHF);
+    qalEffectf(uiEffect, AL_EAXREVERB_GAINLF, RoomParam->flGainLF);
+    qalEffectf(uiEffect, AL_EAXREVERB_DECAY_TIME, RoomParam->flDecayTime);
+    qalEffectf(uiEffect, AL_EAXREVERB_DECAY_HFRATIO, RoomParam->flDecayHFRatio);
+    qalEffectf(uiEffect, AL_EAXREVERB_DECAY_LFRATIO, RoomParam->flDecayLFRatio);
+    qalEffectf(uiEffect, AL_EAXREVERB_REFLECTIONS_GAIN, RoomParam->flReflectionsGain);
+    qalEffectf(uiEffect, AL_EAXREVERB_REFLECTIONS_DELAY, RoomParam->flReflectionsDelay);
+    qalEffectfv(uiEffect, AL_EAXREVERB_REFLECTIONS_PAN, RoomParam->flReflectionsPan);
+    qalEffectf(uiEffect, AL_EAXREVERB_LATE_REVERB_GAIN, RoomParam->flLateReverbGain);
+    qalEffectf(uiEffect, AL_EAXREVERB_LATE_REVERB_DELAY, RoomParam->flLateReverbDelay);
+    qalEffectfv(uiEffect, AL_EAXREVERB_LATE_REVERB_PAN, RoomParam->flLateReverbPan);
+    qalEffectf(uiEffect, AL_EAXREVERB_ECHO_TIME, RoomParam->flEchoTime);
+    qalEffectf(uiEffect, AL_EAXREVERB_ECHO_DEPTH, RoomParam->flEchoDepth);
+    qalEffectf(uiEffect, AL_EAXREVERB_MODULATION_TIME, RoomParam->flModulationTime);
+    qalEffectf(uiEffect, AL_EAXREVERB_MODULATION_DEPTH, RoomParam->flModulationDepth);
+    qalEffectf(uiEffect, AL_EAXREVERB_AIR_ABSORPTION_GAINHF, RoomParam->flAirAbsorptionGainHF);
+    qalEffectf(uiEffect, AL_EAXREVERB_HFREFERENCE, RoomParam->flHFReference);
+    qalEffectf(uiEffect, AL_EAXREVERB_LFREFERENCE, RoomParam->flLFReference);
+    qalEffectf(uiEffect, AL_EAXREVERB_ROOM_ROLLOFF_FACTOR, RoomParam->flRoomRolloffFactor);
+    qalEffecti(uiEffect, AL_EAXREVERB_DECAY_HFLIMIT, (RoomParam->iDecayHFLimit ? AL_TRUE : AL_FALSE));
 
-	if (qalGetError() == AL_NO_ERROR)
-	{
-		bReturn = AL_TRUE;
-	}
+    if (qalGetError() == AL_NO_ERROR)
+    {
+        bReturn = AL_TRUE;
+    }
 
-	return bReturn;
+    return bReturn;
 }
 
 void SX_Init(void)
 {
-	if(!qal_efxinit)
-		return;
+    if (!qal_efxinit)
+        return;
 
-	//Init underwater filter
-	qalGenFilters(1, &alUnderWaterFilter);
-	qalFilteri(alUnderWaterFilter, AL_FILTER_TYPE, AL_FILTER_LOWPASS);
-	qalFilterf(alUnderWaterFilter, AL_LOWPASS_GAIN, AL_LOWPASS_DEFAULT_GAIN);
-	qalFilterf(alUnderWaterFilter, AL_LOWPASS_GAINHF, 0.25f);
+    //Init underwater filter
+    qalGenFilters(1, &alUnderWaterFilter);
+    qalFilteri(alUnderWaterFilter, AL_FILTER_TYPE, AL_FILTER_LOWPASS);
+    qalFilterf(alUnderWaterFilter, AL_LOWPASS_GAIN, AL_LOWPASS_DEFAULT_GAIN);
+    qalFilterf(alUnderWaterFilter, AL_LOWPASS_GAINHF, 0.25f);
 
-	//Init reverb effects
-	qalGenEffects(CSXROOM-1, alReverbEffects);
-	qalGenAuxiliaryEffectSlots(1, &alAuxEffectSlots);
-	for(int i = 0; i < CSXROOM; ++i)
-	{
-		SX_InitReverbEffect(&presets_room[i], alReverbEffects[i]);
-	}
+    //Init reverb effects
+    qalGenEffects(CSXROOM - 1, alReverbEffects);
+    qalGenAuxiliaryEffectSlots(1, &alAuxEffectSlots);
+
+    // Disable reverb when room_type = 0:
+    presets_room[0].flGain = 0;
+    for (int i = 0; i < CSXROOM; ++i)
+    {
+        SX_InitReverbEffect(&presets_room[i], alReverbEffects[i]);
+    }
 }
 
 void SX_Shutdown(void)
 {
-	if(!qal_efxinit)
-		return;
+    if (!qal_efxinit)
+        return;
 
-	qalAuxiliaryEffectSloti(alAuxEffectSlots, AL_EFFECTSLOT_EFFECT, AL_EFFECT_NULL);
-	qalDeleteEffects(CSXROOM-1, alReverbEffects);
-	qalDeleteAuxiliaryEffectSlots(1, &alAuxEffectSlots);
+    qalAuxiliaryEffectSloti(alAuxEffectSlots, AL_EFFECTSLOT_EFFECT, AL_EFFECT_NULL);
+    qalDeleteEffects(CSXROOM - 1, alReverbEffects);
+    qalDeleteAuxiliaryEffectSlots(1, &alAuxEffectSlots);
 
-	qalDeleteFilters(1, &alUnderWaterFilter);
+    qalDeleteFilters(1, &alUnderWaterFilter);
 }
